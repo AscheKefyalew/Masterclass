773b3d57894470c8fb9d2b76472457c1e156a15c
Aschalew Kefyalew <aschalewkefyalew@gmail.com>
2024-07-04 19:08:47 +0300

    separated them with folder

--- Flow_control/Hi_Lo.py
+++ Flow_control/Hi_Lo.py
@@ -5,12 +5,13 @@ print("Plase think of a number between {} and {}".format(low, high))
 input("Press Enter to start")
 
 guesses = 1
-while True:
-    print("\tGuessing in the range of {} to {}".format(low, high))
+while low != high:  # or we can use True
+    # print("\tGuessing in the range of {} to {}".format(low, high))
     guess = low + (high - low) // 2
     high_low = input("My guess is {}. Should I guess higher or lower? "
                      "Enter h or l, or c is my guess was correct "
                      .format(guess)).casefold()
+    
     if high_low == "h":
         # Guess higher. The low end of the range becomes 1 greater than the guess.
         low = guess + 1
@@ -20,9 +21,11 @@ while True:
     elif high_low == "c":
         print("I got it in {} guesses!".format(guesses))
         break
-
     else:
         print("Please enter h, l or c")
     
     # guesses = guesses + 1
     guesses += 1
+else:
+    print("You though of the number {}".format(low))
+    print("I got it in {} guesses".format(guesses))
