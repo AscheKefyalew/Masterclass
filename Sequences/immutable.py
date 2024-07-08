b70d1613315facef8a55a20da42b808a3d29aaeb
Aschalew Kefyalew <aschalewkefyalew@gmail.com>
2024-07-08 18:59:38 +0300

    sequence

---
+++ Sequences/immutable.py
@@ -0,0 +1,19 @@
+# immutabele means it can't be changed.
+
+
+# result = True
+# another_result = result
+# print(id(result))
+# print(id(another_result))
+
+# result = False
+# print(id(result))
+
+result = "Correct"
+another_result = result
+print(id(result))
+print(id(another_result))
+
+result += "ish"
+print(id(result))
+print(id(another_result))
