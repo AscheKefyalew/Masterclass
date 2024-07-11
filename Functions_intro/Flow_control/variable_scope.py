012b01e6810ec71e4767e65270bdaab7f4e594c9
Aschalew Kefyalew <aschalewkefyalew@gmail.com>
2024-07-11 18:57:10 +0300

    functions

--- Functions_intro/Flow_control/variable_scope.py
+++ Functions_intro/Flow_control/variable_scope.py
@@ -6,9 +6,9 @@ def outer_func():
         z = 'local'
         print(x)
         print(y)
-        print(z)
+        print(z)         # The inner function can acces the outer function.
     inner_func()
 
 outer_func()
 print(x)
-print(y)
+# print(y)               # The outer function can not access the inner function but the inner function can access for the outer one.
